366ca1c899ae87f3c29fa175e073b272f7c71ab2
Aleksander-Kovalenko <coldfellow@gmail.com>
2021-01-16 20:03:49 +0200

    config btn loadMore

--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
 import './styles.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
+import LoadMoreBtn from './js/load-more-bnt.js';
 import ServerApi from './js/apiServer.js';
 
 import template from './template/template.hbs';
 
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+
 const API = new ServerApi();
 
-const refs = {
+const ref = {
   cardBox: document.querySelector('.gallery'),
   form: document.querySelector('.search-form'),
-  loadMore: document.querySelector('.btn'),
+  submit: document.querySelector('.btn'),
 };
 
-refs.form.addEventListener('submit', onSearchQuery);
-refs.loadMore.addEventListener('click', onLoadMore);
+ref.form.addEventListener('submit', onSearchQuery);
+loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
 
 function onSearchQuery(e) {
   e.preventDefault();
+
+  loadMoreBtn.show();
   let query = e.currentTarget.elements.query.value;
 
   if (query === '') {
@@ -26,18 +37,20 @@ function onSearchQuery(e) {
   API.query = query;
   API.getFetch().then(renderList);
 
-  onClearInput();
+  onResetSearch();
 }
 function onLoadMore() {
+  loadMoreBtn.disable();
   API.incrementPage();
   API.getFetch().then(renderList);
 }
 
-function onClearInput() {
-  return refs.form.reset();
+function onResetSearch() {
+  ref.form.reset();
+  ref.cardBox.innerHTML = '';
 }
 
 function renderList(searchArr) {
   const templateCard = template(searchArr);
-  refs.cardBox.insertAdjacentHTML('beforeend', templateCard);
+  ref.cardBox.insertAdjacentHTML('beforeend', templateCard);
 }
