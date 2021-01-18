66bbcea560e46758c553e32ece0ea05656e437dd
Aleksander-Kovalenko <coldfellow@gmail.com>
2021-01-18 09:34:09 +0200

    config scroll

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ function onLoadMore() {
   API.getFetch().then(response => {
     renderList(response);
     window.scrollTo({
-      top: document.documentElement.clientHeight + scroll,
+      top: document.documentElement.clientHeight,
       behavior: 'smooth',
     });
   });
